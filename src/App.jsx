6449583dcc88b6332e6d233c460ef4f8cdb523de
Xavier-Hsiao <tim88776@gmail.com>
2024-02-26 17:29:31 +0800

    Build: add Row styled component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
   padding: 20px;
-  background-color: orangered;
 `;
 
 function App() {
@@ -18,11 +14,26 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hello world!</H1>
-        <Button onClick={() => alert("Check in baby!")}>Check in</Button>
-        <Button onClick={() => alert("Check out baby!")}>Check out</Button>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in baby!")}>Check in</Button>
+              <Button onClick={() => alert("Check out baby!")}>
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Input type="number" placeholder="Number of guests" />
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
