bb45602a769b9fbc9212d4eff4df4f7907669af6
Xavier-Hsiao <tim88776@gmail.com>
2024-03-14 14:23:12 +0800

    Feat: get current settings on form

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,38 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import { useSettings } from "./useSettings";
 
 function UpdateSettingsForm() {
+  const { isLoading, settings } = useSettings();
+  const {
+    min_booking_length: minBookingLength,
+    max_booking_length: maxBookingLength,
+    max_guests_per_booking: maxQuestsPerBooking,
+    breakfast_price: breakfastPrice,
+  } = settings;
+
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow label="Maximum nights/booking">
+        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow label="Maximum guests/booking">
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={maxQuestsPerBooking}
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price">
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfastPrice}
+        />
       </FormRow>
     </Form>
   );
